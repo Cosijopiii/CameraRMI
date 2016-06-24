e81b0d79a024050f446dfb45a96ad028a6301e5c
Cosijopii Garcia Garcia <kosi.cosi@gmail.com>
2016-06-24 09:22:55 -0500

    make skeleton for app

--- src/ClientSide/Main.java
+++ src/ClientSide/Main.java
@@ -10,13 +10,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("VistaPrincipal.fxml"));
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
-
-
     public static void main(String[] args) {
         launch(args);
     }
